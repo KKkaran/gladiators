31b2d4da8f468952628f3419df3714ea9f70134b
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-10-23 19:04:59 -0400

    created some functions and vars

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,25 @@
-window.alert("this is a feature/initial-game branch")
-console.log("this is a feature/inital branch")
+let input = window.prompt("hit me with a number!!")
+if(input < 5){
+    console.log("input is less than 5")
+}else if(input == 5){
+    console.log("exactly same")
+}else{
+    console.log("input is greater than 5")
+}
+
+console.log("the input was " + input)
+
+
+
+
+
+
+
+
+
+
+
+//this is a line comment
+/*
+this is a block comment
+*/
